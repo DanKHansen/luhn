3d797d2fff5f027dacd9f3ea5ebc3d2ac285ffbb
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-06 12:25:01 +0200

    refactoring

--- src/main/scala/Luhn.scala
+++ src/main/scala/Luhn.scala
@@ -1,9 +1,8 @@
 object Luhn:
    def valid(s: String): Boolean =
       val trimmed = s.filterNot(_.isSpaceChar)
-      val okToValidate = trimmed.forall(_.isDigit) && trimmed.length > 1
-      okToValidate && {
-         val everySecond = trimmed.zip(trimmed.indices.reverse).filter(t => t._2 % 2 != 0).map(_._1).mkString
+      trimmed.forall(_.isDigit) && trimmed.length > 1 && {
+         val everySecond = trimmed.reverse.zipWithIndex.filter(_._2 % 2 != 0).map(_._1)
          val doubled = everySecond.map(_.asDigit).map(n => if n * 2 > 9 then n * 2 - 9 else n * 2).mkString
          trimmed.diff(everySecond).concat(doubled).map(_.asDigit).sum % 10 == 0
       }
