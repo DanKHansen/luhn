b3248647053059378e6d57b72a9727464fff935b
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-06 12:00:13 +0200

    solution

--- src/main/scala/Luhn.scala
+++ src/main/scala/Luhn.scala
@@ -1,2 +1,9 @@
 object Luhn:
-   def valid(s: String): Boolean = ???
+   def valid(s: String): Boolean =
+      val trimmed = s.filterNot(_.isSpaceChar)
+      val okToValidate = trimmed.forall(_.isDigit) && trimmed.length > 1
+      okToValidate && {
+         val everySecond = trimmed.zip(trimmed.indices.reverse).filter(t => t._2 % 2 != 0).map(_._1).mkString
+         val doubled = everySecond.map(_.asDigit).map(n => if n * 2 > 9 then n * 2 - 9 else n * 2).mkString
+         trimmed.diff(everySecond).concat(doubled).map(_.asDigit).sum % 10 == 0
+      }
